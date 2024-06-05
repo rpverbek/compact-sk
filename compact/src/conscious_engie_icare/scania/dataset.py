8c899840050e950f353cb7ffb6be405624f84d37
FFNG <fabian.fingerhut@sirris.be>
2024-06-05 11:19:24 +0200

    new folder structure for scania use case

--- compact/src/conscious_engie_icare/scania/dataset.py
+++ compact/src/conscious_engie_icare/scania/dataset.py
@@ -1,6 +1,6 @@
 import os 
 
-DATA_PATH = os.path.join('..', 'data', 'IDA 2024 Industrial Challenge')
+DATA_PATH = os.path.join('..', '..', 'data', 'IDA 2024 Industrial Challenge')
 TRAIN_OPERATIONAL_PATH = os.path.join(DATA_PATH, 'train_operational_readouts.csv')
 TRAIN_REPAIR_PATH = os.path.join(DATA_PATH, 'train_tte.csv')
 TRAIN_SPECIFICATIONS = os.path.join(DATA_PATH, 'train_specifications.csv')
