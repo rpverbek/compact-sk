0246b2600006d15a0fdf6284241fa272370c2467
FFNG <fabian.fingerhut@sirris.be>
2024-06-06 15:48:25 +0200

    update volvo datasets

---
+++ compact/src/conscious_engie_icare/volvo/dataset.py
@@ -0,0 +1,6 @@
+import os 
+
+DATA_PATH = os.path.join('..', 'data', 'Volvo Challenge ECML 2024')
+TRAIN_PATH = os.path.join(DATA_PATH, 'train_gen1.csv')
+TEST_PATH = os.path.join(DATA_PATH, 'public_X_test.csv')
+VARIANTS_PATH = os.path.join(DATA_PATH, 'variants.csv')
