428b4c51962ac155e37dab135ed28e8437fd26f2
Robbert Verbeke <50441465+rpverbek@users.noreply.github.com>
2024-07-04 17:16:01 +0200

    Added interactive starter kit with first interactive function

--- compact/src/conscious_engie_icare/viz/viz.py
+++ compact/src/conscious_engie_icare/viz/viz.py
@@ -7,13 +7,18 @@ Utilities for visualisations.
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
+from sklearn.decomposition import PCA
 import matplotlib as mpl
 import matplotlib.dates as mdates
 from matplotlib.ticker import NullFormatter
 import matplotlib.ticker as ticker
 import re
 import numpy as np
+import kneed
+from ipywidgets import interact, Layout
+import ipywidgets as widgets
+from copy import deepcopy
+from matplotlib.gridspec import GridSpec
 
 
 def illustrate_nmf_components_for_paper(V, explained_variance_ratio, df_nmf_models, band_cols,
@@ -147,6 +152,201 @@ def illustrate_nmf_components_for_paper(V, explained_variance_ratio, df_nmf_mode
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
     # ax.set_xlim(xlims)
     return fig, ax_row
+
+
+def get_number_of_components(explained_variance, threshold, df_nmf_models_):
+    if np.max(explained_variance) < threshold:
+        n_components_variance = len(explained_variance)
+    else:
+        n_components_variance = (explained_variance > threshold).argmax() + 1
+
+    kneedle = kneed.KneeLocator(x=np.arange(1, len(explained_variance) + 1), y=explained_variance,
+                                direction='increasing', S=0)
+    n_components_knee_pca = kneedle.knee
+
+    kneedle = kneed.KneeLocator(x=df_nmf_models_['n_components'],
+                                y=df_nmf_models_['reconstruction_error'],
+                                direction='decreasing', curve='convex')
+    n_components_knee_nmf = kneedle.elbow
+
+    return n_components_variance, n_components_knee_pca, n_components_knee_nmf
+
+
+def extract_lower_limit(x):
+    return float(re.findall(r'(?<=\_)(.*)(?=-)', x)[0])
+
+
+def extract_upper_limit(x):
+    return float(re.findall(r'(?<=-)(.*)$', x)[0])
+
+
+def get_controller(controller):
+    """
+    Parse dictionary into widget.
+
+    :param controller: dict. Dictionary with inputs to widget. Must have at
+    least key indicating name of widget
+    :return:
+    widget
+    """
+    controller = deepcopy(controller)
+    wdgt = controller['widget']
+
+    controller.pop('widget')
+    controller['continuous_update'] = controller.get('continuous_update',
+                                                     False)
+    controller['style'] = controller.get('style',
+                                         {'description_width': 'initial'})
+    controller['layout'] = Layout(width='60%')
+    controller_out = getattr(widgets, wdgt)(**controller)
+    return controller_out
+
+
+def illustrate_nmf_components_interactive(df_V_train_folds, df_nmf_models_folds):
+    global previous_value_threshold
+    previous_value_threshold = 95
+
+    max_n_components = len(df_nmf_models_folds[0])
+
+    def make_plot(fold, criterium, n_components_range, **extra_options):
+        global previous_value_threshold
+        df_V_train_ = df_V_train_folds[fold]
+        df_nmf_models_ = df_nmf_models_folds[fold]
+
+        # calculate explained variance
+        pca = PCA(n_components=max_n_components, random_state=42)
+        pca.fit(df_V_train_)
+        explained_variance_ratio = pca.explained_variance_ratio_.cumsum()
+        explained_variance_ratio = explained_variance_ratio[:max_n_components] * 100
+
+        if 'min_explained_variance' in extra_options.keys():
+            threshold = extra_options['min_explained_variance']
+        else:
+            threshold = previous_value_threshold
+
+        previous_value_threshold = threshold
+
+        n_components_variance, n_components_knee_pca, n_components_knee_nmf = get_number_of_components(
+            explained_variance_ratio,
+            threshold,
+            df_nmf_models_)
+
+        if criterium == 'explained-variance':
+            n_components = n_components_variance
+        if criterium == 'knee-point':
+            n_components = max(n_components_knee_nmf, n_components_knee_pca)
+        if criterium == 'both':
+            n_components = max(n_components_variance, n_components_knee_nmf, n_components_knee_pca)
+        else:
+            n_components = 5
+
+        n_components = max(n_components, n_components_range[0])
+        n_components = min(n_components, n_components_range[1])
+
+        components_per_row = 3
+        n_rows_first_plot = 3
+        n_rows = n_rows_first_plot + int(np.ceil(n_components / components_per_row))
+        n_cols = 2 * components_per_row
+
+        fig = plt.figure(layout="constrained", figsize=(12, n_rows))
+
+        gs = GridSpec(n_rows, n_cols, figure=fig)
+        ax1 = fig.add_subplot(gs[:n_rows_first_plot, :n_cols // 2])
+        ax2 = fig.add_subplot(gs[:n_rows_first_plot, n_cols // 2:])
+        # identical to ax1 = plt.subplot(gs.new_subplotspec((0, 0), colspan=3))
+
+        ax1.plot(np.arange(1, max_n_components + 1), explained_variance_ratio, 'ko-')
+        ax2.plot(df_nmf_models_['n_components'],
+                 df_nmf_models_['reconstruction_error'], 'ko-')
+
+        if criterium in ('both', 'explained-variance'):
+            ax1.axhline(y=extra_options['min_explained_variance'], linestyle=':', color='g')
+            ax1.axvline(x=n_components_variance, linestyle=':', color='g',
+                        label=f'Explained variance PCA (N={n_components_variance})')
+        if criterium in ('both', 'knee-point'):
+            ax1.axvline(x=n_components_knee_pca, linestyle=':', color='b',
+                        label=f'Knee point PCA (N={n_components_knee_pca})')
+            ax2.axvline(x=n_components_knee_nmf, linestyle=':', color='r',
+                        label=f'Knee point NMF (N={n_components_knee_nmf})')
+            ax2.legend(loc='upper right', fontsize='small')
+        ax1.set_xlabel("Number of components")
+        ax1.set_ylabel("Cumulative explained variance")
+        ax2.set_xlabel("Number of components")
+        ax2.set_ylabel("Frobenium norm")
+
+        ax1.legend(loc='lower right', fontsize='small')
+
+        row = df_nmf_models_[df_nmf_models_.n_components == n_components].iloc[0]
+        H = row.H
+
+        axes_components = []
+
+        column_names = df_V_train_.columns[0]
+        offset = 0.5 * (extract_upper_limit(column_names) - extract_lower_limit(column_names))
+        all_x_label_names = [extract_lower_limit(col) + offset for col in df_V_train_.columns]
+        plot_x_ticks = [0, 10, 20, 30, 40, 49]
+        plot_x_labels = [all_x_label_names[idx] for idx in plot_x_ticks]
+
+        for _i_component in range(n_components):
+            i_row = n_rows_first_plot + (_i_component // components_per_row)
+            i_col = 2 * (_i_component % components_per_row)
+
+            if len(axes_components) > 0:
+                _ax = fig.add_subplot(gs[i_row, i_col:i_col + 2],
+                                      sharey=axes_components[0], sharex=axes_components[0])
+            else:
+                _ax = fig.add_subplot(gs[i_row, i_col:i_col + 2])
+
+            _ax.plot(np.arange(H.shape[1]), H.iloc[_i_component])
+            _ax.text(0, 2.5, f'Component {_i_component + 1}', fontdict={'size': 10})
+            _ax.set_xticks(plot_x_ticks)
+            _ax.set_xticklabels(plot_x_labels)
+
+            axes_components.append(_ax)
+            if i_row == (n_rows - 1):
+                _ax.set_xlabel('Frequency [orders]')
+
+        fig.show()
+
+    def get_additional_options(fold, criterium, n_components_range):
+        global previous_value_threshold
+        threshold = 95 if previous_value_threshold is None else previous_value_threshold
+        if criterium in ('explained-variance', 'both'):
+            controller_explained_variance = get_controller({'widget': 'FloatSlider',
+                                                            'min': 0,
+                                                            'max': 100,
+                                                            'value': threshold,
+                                                            'description': 'Explained PCA variance threshold'})
+            extra_options = {'min_explained_variance': controller_explained_variance}
+        else:
+            extra_options = {}
+
+        def _make_plot(**_extra_options):
+            make_plot(fold, criterium, n_components_range, **_extra_options)
+
+        interact(_make_plot, **extra_options)
+
+    controller_selection_criterium = get_controller({'widget': 'Dropdown',
+                                                     'options': [('Explained variance', 'explained-variance'),
+                                                                 ('Knee point', 'knee-point'),
+                                                                 ('Both', 'both')],
+                                                     'value': 'explained-variance',
+                                                     'description': 'How to select the number of components for NMF'})
+
+    controller_n_components_range = get_controller({'widget': 'IntRangeSlider',
+                                                    'min': 1,
+                                                    'max': max_n_components,
+                                                    'value': [2, 10],
+                                                    'description': 'Range of acceptable number of components for NMF'})
+
+    controller_fold = get_controller({'widget': 'IntSlider',
+                                      'min': 0,
+                                      'max': len(df_V_train_folds),
+                                      'value': 0,
+                                      'description': 'Fold number'})
+
+    interact(get_additional_options, fold=controller_fold, criterium=controller_selection_criterium,
+             n_components_range=controller_n_components_range)
 
 
 def illustrate_nmf_components(V, explained_variance_ratio, df_nmf_models, band_cols, min_explained_variance=90,
