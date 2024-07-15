2b506dc5b160b1e3065cb01907461742a8c8c910
FFNG <fabian.fingerhut@sirris.be>
2024-07-15 10:04:34 +0000

    update source code Scania

--- compact/src/conscious_engie_icare/scania/dataset.py
+++ compact/src/conscious_engie_icare/scania/dataset.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import pandas as pd
 
 DATA_PATH = os.path.join('..', '..', 'data', 'IDA 2024 Industrial Challenge')
 TRAIN_OPERATIONAL_PATH = os.path.join(DATA_PATH, 'train_operational_readouts.csv')
@@ -7,3 +8,22 @@ TRAIN_SPECIFICATIONS = os.path.join(DATA_PATH, 'train_specifications.csv')
 
 TEST_PATH = os.path.join(DATA_PATH, 'public_X_test.csv')
 VARIANTS_PATH = os.path.join(DATA_PATH, 'variants.csv')
+
+
+def load_and_preprocess_specifications(data_path=TRAIN_SPECIFICATIONS):
+    df_specifications = pd.read_csv(data_path, index_col='vehicle_id')
+    df_specifications = add_column_prefix(df_specifications)
+    return df_specifications
+
+
+def add_column_prefix(df):
+    """ Add specification prefix for truck specifications. """
+    for col in df.columns:
+        df[col] = df[col].apply(lambda x: f'{col}_{x}')
+    return df
+
+
+def get_attribute_columns(df_ts):
+    ATTRIBUTE_COLUMNS = df_ts.columns.str.extract(r'(\d+_\d+)')[0]
+    ATTRIBUTE_COLUMNS = ATTRIBUTE_COLUMNS[~ATTRIBUTE_COLUMNS.isna()].tolist()
+    return ATTRIBUTE_COLUMNS
--- compact/src/conscious_engie_icare/scania/viz.py
+++ compact/src/conscious_engie_icare/scania/viz.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
+from conscious_engie_icare.scania.dataset import get_attribute_columns
+import matplotlib as mpl
 
 
-def plot_cumulative_timeseries(df, vehicle_ids):
-    unique_features = set(int(col.split('_')[0]) for col in ATTRIBUTE_COLUMNS)
+def plot_cumulative_timeseries(df, vehicle_ids, attribute_columns=None):
+    if attribute_columns is None:
+        attribute_columns = get_attribute_columns(df)
+    
+    unique_features = set(int(col.split('_')[0]) for col in attribute_columns)
     fig, axes = plt.subplots(figsize=(6*len(unique_features), 4*len(vehicle_ids)),
                              nrows=len(vehicle_ids), ncols=len(unique_features), sharex=True)
     if len(vehicle_ids) > 1:
@@ -25,3 +30,23 @@ def plot_cumulative_timeseries(df, vehicle_ids):
             ax.set_title(feature)
     fig.tight_layout()
     return fig, axes
+
+
+def plot_V(V):
+    cmap = mpl.cm.get_cmap("Blues")
+    fig, ax = plt.subplots(figsize=(6, 8))
+    nrows = V.shape[0]
+    ncols = V.shape[1]
+    title_ = "Performance matrix V" + f" ({nrows} x {ncols})"
+    ax.set_title(title_, fontsize=20)
+    # V.columns = BAND_COLUMNS
+    im = ax.imshow(
+        V,
+        cmap=cmap,
+        aspect='auto',
+        interpolation='nearest',
+        norm=mpl.colors.LogNorm(vmin=None, vmax=None),
+        # extent=[0.25,30.25,nrows,0]
+    )
+    ax.tick_params(axis='y', labelrotation=90)
+    return fig, ax
