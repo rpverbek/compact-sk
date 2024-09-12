35db2bede21ccdd535721db216879673e5fbe844
Robbert Verbeke <50441465+rpverbek@users.noreply.github.com>
2024-09-12 13:53:50 +0200

    moved setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='compact',
+    packages=find_packages(),
+    version='0.0.1',
+    description='COMPACT project code',
+    author='ffng',
+    install_requires=[]
+)
